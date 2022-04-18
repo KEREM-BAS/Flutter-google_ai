c44c2b8f9c4429be37fad2f5c1e3f4894e5c31cf
KEREM BAS <kerembas2000@gmail.com>
2022-04-18 19:51:37 +0300

    master

---
+++ lib/views/homepage.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
